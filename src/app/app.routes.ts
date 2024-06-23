7632714add51dc55abe54294132c883fea2bde70
BaeNouvelleVie <i202122429@cibertec.edu.pe>
2024-06-22 20:15:10 -0500

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './auth/login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './dashboard/home/home.component';
+import { Pregunta2Component } from './dashboard/pregunta2/pregunta2.component';
+import { Pregunta3Component } from './dashboard/pregunta3/pregunta3.component';
+import { Pregunta4Component } from './dashboard/pregunta4/pregunta4.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path: "login", component: LoginComponent},
+    {path: "dashboard", component: DashboardComponent,
+
+        //FALTA UN GUARD
+        children: [
+            {path: "home", component: HomeComponent},
+            {path: "pregunta2", component: Pregunta2Component},
+            {path: "pregunta3", component: Pregunta3Component},
+            {path: "pregunta4", component: Pregunta4Component},
+        ]
+    },
+     
+    {path: "", redirectTo: "login", pathMatch: "full"},
+    {path: "**", component:PageNotFoundComponent}
+
+];
---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MaterialModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
