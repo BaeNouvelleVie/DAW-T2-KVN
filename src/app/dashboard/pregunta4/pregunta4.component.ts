18108b25477b63ddbe4550f492ef239576af506f
BaeNouvelleVie <i202122429@cibertec.edu.pe>
2024-06-22 21:25:03 -0500

    Pregunta 4 finalizada, osea Todo finalizad0

--- src/app/dashboard/pregunta4/pregunta4.component.ts
+++ src/app/dashboard/pregunta4/pregunta4.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
+import { MaterialModule } from '../../angular-material/material/material.module';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pregunta4',
   standalone: true,
-  imports: [],
+  imports: [MaterialModule,FormsModule,CommonModule],
   templateUrl: './pregunta4.component.html',
   styleUrl: './pregunta4.component.css'
 })
-export class Pregunta4Component {
 
+export class Pregunta4Component {
+  precioUnitario: number = 0;
+  cantidad: number = 0;
+  totalConDescuento: number = -1;
+
+  calcularTotal(): void {
+    let total = this.precioUnitario * this.cantidad;
+    if (total > 200) {
+      this.totalConDescuento = total * 0.8; 
+    } else {
+      this.totalConDescuento = total; 
+    }
+  }
 }
